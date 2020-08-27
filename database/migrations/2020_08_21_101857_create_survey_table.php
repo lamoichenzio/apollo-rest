a389bf4bd376fe87c8d7d6e4379fbf037d8b81fa
Davide <dav.romano@icloud.com>
2020-08-27 23:57:22 +0200

    fix survey table

--- database/migrations/2020_08_21_101857_create_survey_table.php
+++ database/migrations/2020_08_21_101857_create_survey_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSurveyTable extends Migration
+class CreateSurveysTable extends Migration
 {
     /**
      * Run the migrations.
@@ -41,6 +41,6 @@ class CreateSurveyTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('survey');
+        Schema::dropIfExists('surveys');
     }
 }
