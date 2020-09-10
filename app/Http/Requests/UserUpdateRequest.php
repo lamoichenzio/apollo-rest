1c40562d772c9118dc550905e44b1f85a01b995b
GIordano <giordano.daloisio@gmail.com>
2020-09-10 17:04:34 +0200

    fix unique email validation in user update

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 
 class UserUpdateRequest extends FormRequest
 {
@@ -24,11 +25,12 @@ class UserUpdateRequest extends FormRequest
      */
     public function rules()
     {
+        $user = $this->route('user');
         return [
             'username' => 'sometimes|required|min:4',
             'password' => 'sometimes|required|min:5',
             'old_password' => 'required_with:password|password',
-            'email' => 'sometimes|required|email|unique:users',
+            'email' => 'sometimes|required|email|' . Rule::unique('users')->ignore($user->id),
             'avatar' => 'sometimes|required',
             'avatar.name' => 'sometimes|required_with:avatar|required_unless:avatar,delete|string',
             'avatar.data' => 'sometimes|required_with:avatar|required_unless:avatar,delete|base64image|base64max:5000'
