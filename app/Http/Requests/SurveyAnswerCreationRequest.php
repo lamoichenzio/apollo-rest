997f917046cf9b0942547be8204b0867a02dce55
GIordano <giordano.daloisio@gmail.com>
2020-10-06 11:52:35 +0200

    fix

--- app/Http/Requests/SurveyAnswerCreationRequest.php
+++ app/Http/Requests/SurveyAnswerCreationRequest.php
@@ -38,21 +38,21 @@ class SurveyAnswerCreationRequest extends FormRequest
                 return $survey->secret;
             })],
             'answers' => ['required', 'array', new RequiredAnswer()],
-            'answers.*.question_id' => 'required',
-            'answers.*.question_type' => 'required|' . Rule::in([
+            'answers.*.questionId' => 'required',
+            'answers.*.questionType' => 'required|' . Rule::in([
                     InputQuestion::class, MultiQuestion::class, MatrixQuestion::class]),
             'answers.*.answer' => [
                 'string',
                 'required_without_all:answers.*.answers,answers.*.answer_pair,answers.*.answers_pair'],
             'answers.*.answers' => 'array|required_without_all:answers.*.answer,answers.*.answer_pair,answers.*.answers_pair',
             'answers.*.answers.*' => 'sometimes|required|string',
-            'answers.*.answer_pair' => 'array|required_without_all:answers.*.answers,answers.*.answer,answers.*.answers_pair',
-            'answers.*.answer_pair.*.element' => 'required|exists:matrix_question_elements,id',
-            'answers.*.answer_pair.*.answer' => 'required|string',
-            'answers.*.answers_pair' => 'array|required_without_all:answers.*.answers,answers.*.answer_pair,answers.*.answer',
-            'answers.*.answers_pair.*.element' => 'required|exists:matrix_question_elements,id',
-            'answers.*.answers_pair.*.answers' => 'required|array',
-            'answers.*.answers_pair.*.answers:*' => 'string',
+            'answers.*.answerPair' => 'array|required_without_all:answers.*.answers,answers.*.answer,answers.*.answers_pair',
+            'answers.*.answerPair.*.element' => 'required|exists:matrix_question_elements,id',
+            'answers.*.answerPair.*.answer' => 'required|string',
+            'answers.*.answersPair' => 'array|required_without_all:answers.*.answers,answers.*.answer_pair,answers.*.answer',
+            'answers.*.answersPair.*.element' => 'required|exists:matrix_question_elements,id',
+            'answers.*.answersPair.*.answers' => 'required|array',
+            'answers.*.answersPair.*.answers:*' => 'string',
         ];
     }
 }
